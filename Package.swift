0b98edf40d400d5bedfb31f3fc8de26c7bf39218
Joseph <joseph.thecouncil@gmail.com>
2026-06-23 01:56:36 +0300

    Pin CouncilKit with .upToNextMinor (matches the README's guidance)

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ let package = Package(
     name: "CouncilKitExample",
     platforms: [.macOS(.v14)],
     dependencies: [
-        .package(url: "https://github.com/albertofettucini/CouncilKit.git", from: "0.1.0"),
+        .package(url: "https://github.com/albertofettucini/CouncilKit.git", .upToNextMinor(from: "0.1.0")),
     ],
     targets: [
         .executableTarget(
